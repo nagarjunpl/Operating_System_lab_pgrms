a96c294aa2313e496e20fb7afc1d42977c27473a
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-12-10 19:01:58 +0530

    Refactor Lseek system call example code

--- Lseek()_System_call.c
+++ Lseek()_System_call.c
@@ -1,30 +1,20 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<stdio.h>
 
 int main()
 {
-    int fd, pos;
-    char buff[11];     // +1 for safety (null termination not required)
-
-    fd = open("seeking", O_RDONLY);
-    if (fd < 0) {
-        perror("Error opening file");
-        return 1;
-    }
-
-    // Move pointer to 10th byte (index 10)
-    pos = lseek(fd, 10, SEEK_SET);
-    printf("Pointer is at %d position\n", pos);
-
-    // Read 10 characters from 10th position
-    read(fd, buff, 10);
     
-    // Write these 10 characters to screen
-    write(1, buff, 10);
-
-    close(fd);
-    return 0;
+    int n,f,f1;
+    char buff[10];
+    
+    f=open("seeking",O_RDWR);
+    f1=lseek(f,10,SEEK_SET);
+    
+    printf("Pointer is at %d position\n",f1);
+    
+    read(f,buff,10);
+    write(1,buff,10);
 }
