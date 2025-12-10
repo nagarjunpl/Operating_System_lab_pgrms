c53fec054f6b2154603335f9719a14a429893945
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-12-10 19:07:22 +0530

    Refactor shared memory reader implementation

--- Reader_SharedMemory.c
+++ Reader_SharedMemory.c
@@ -1,39 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/shm.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<string.h>
 
 int main()
-{
-    int shmid;
+    {
+    int i;
     void *shared_memory;
-
-    // Access the existing shared memory (do NOT use IPC_CREAT)
-    shmid = shmget((key_t)2345, 1024, 0666);
-    if (shmid == -1) {
-        perror("shmget failed");
-        return 1;
-    }
-
-    printf("Shared memory key (ID): %d\n", shmid);
-
-    // Attach memory
-    shared_memory = shmat(shmid, NULL, 0);
-    if (shared_memory == (void*)-1) {
-        perror("shmat failed");
-        return 1;
-    }
-
-    printf("Process attached at: %p\n", shared_memory);
-
-    printf("Data read from shared memory: %s\n", (char*)shared_memory);
-
-    // Detach
-    shmdt(shared_memory);
-
-    // Optional: delete shared memory
-    // shmctl(shmid, IPC_RMID, NULL);
-
-    return 0;
+    char buff[100];
+    int shmid;
+        
+    shmid=shmget((key_t)2345, 1024, 0666);
+        
+    printf("Key of shared memory is %d\n",shmid);
+    shared_memory=shmat(shmid,NULL,0); //process attached to shared memory segment
+    printf("Process attached at %p\n",shared_memory);
+        
+    printf("Data read from shared memory is : %s\n",(char *)shared_memory);
 }
